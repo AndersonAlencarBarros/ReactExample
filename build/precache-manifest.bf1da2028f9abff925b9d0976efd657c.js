2a3d2f4a898b8198f67fa043ee33516e0bcca497
AAB <AAB@CECOMP.UNIVASF.EDU.BR>
2019-11-08 10:58:29 -0300

    States and lifecycle

---
+++ build/precache-manifest.bf1da2028f9abff925b9d0976efd657c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b51ae41862df0b9ac429e1ab651fdb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a23510e6a3770a4e728",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "43b044ede722ac539b64",
+    "url": "/static/js/2.d91a6eed.chunk.js"
+  },
+  {
+    "revision": "8a23510e6a3770a4e728",
+    "url": "/static/js/main.ee58caee.chunk.js"
+  },
+  {
+    "revision": "867559b179ea8c7af62a",
+    "url": "/static/js/runtime-main.6a03bc6f.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -4,15 +4,41 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// <App /> funcao principal que contem todos os 
+// os outros componentes
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 function formatName(user){
     return user.firstName + ' ' + user.lastName;
 }
 
+const user = {
+    firstName: 'Anderson',
+    lastName: 'Alencar'
+}
+
+const sunset = (
+    <div> 
+        <div> 
+            <h1><em>Sunset</em></h1>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.dWfOYXNXp-qIuJGY4yQ3JAHaHa%26pid%3DApi&f=1" />
+        </div>
+    </div>
+);
+
+// Exemplo do uso de 'props':
+// A seguir sera definida um componente 'BoasVindas'
+// que receberao 'name' e 'last' como props
 function BoasVindas (props){
     return <h5>Bem Vindo, {props.name} {props.last}</h5>;
 }
+
+// define um componente do usuario 'BoasVindas' 
+// como uma a funcao predefinida
+// e envia os atributos 'name' e 'last',
+// esses atributos sao chamados de 'props'  
+const welcome = <BoasVindas name="Anderson" last="Alencar" /> 
+
 // ou, de forma equivalente
 // class Welcome extends React.Component{
 //     render(){
@@ -20,47 +46,92 @@ function BoasVindas (props){
 //     }
 // }
 
-function App (){ // a funcao 'App' por padrao é definida uma vez e usada para chamar todos os outros componentes
+// a funcao 'App' por padrao é definida uma vez 
+// e usada para chamar todos os outros componentes
+function App (){
     return(
-        <div> 
-        <div> 
-            <h1><em>{greetings}</em></h1>
-            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.dWfOYXNXp-qIuJGY4yQ3JAHaHa%26pid%3DApi&f=1" />
-        </div>
-        
-        <div>
-            <BoasVindas name="Paulo" />
-            <BoasVindas name="Ana" />
-            <BoasVindas name="John" />
-        </div>
+        <div>             
+            <div> 
+                <h1><em>{greetings}</em></h1>
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.dWfOYXNXp-qIuJGY4yQ3JAHaHa%26pid%3DApi&f=1" />
+            </div>
+            
+            <div>
+                <BoasVindas name="AeroNildo" />
+                <BoasVindas name="Ana" />
+                <BoasVindas name="John" />
+            </div>
+
+            <div>
+                <Clock />
+            </div>
+            
+            <div>    
+                <ActionLink />
+            </div>
         </div>
     );
 }
- 
- 
 
-const welcome = <BoasVindas name="Anderson" last="Alencar" /> // define componente do usuario, o atributo é passado para a funcao pre-definida, esses atributos sao chamados de 'props'
-
-const user = {
-    firstName: 'Anderson',
-    lastName: 'Alencar'
+function ActionLink() {
+    function handleClick(e) {
+      e.preventDefault();
+      console.log('The link was clicked.');
+    }
+  
+    return (
+    <button>
+      <a href="#" onClick={handleClick}>
+        Click me
+      </a>
+      </button>
+    );
 }
 
+// Equivalente a <h1 className="title">Sunset</h1>
 const greetings = React.createElement(
     'h1',
     {className: 'title'},
     'Sunset'
 );
 
-const sunset = (
-    <div> 
-        <div> 
-            <h1><em>{greetings}</em></h1>
-            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.dWfOYXNXp-qIuJGY4yQ3JAHaHa%26pid%3DApi&f=1" />
-        </div>
-    </div>
-);
+// Exemplo de State e Lifecycle
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
 
+    // Lifecycle methods - destruir componentes 
+    // 'componentDidMount' executa quando o componente 
+    // é renderizado na DOM 
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        );
+    }
+
+    // Executando quando o componente é removido da DOM
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+
+    // setState() é responsavel por atualizar o state.
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+  
+    render() {
+      return (
+        <div>   
+          <h5> São {this.state.date.toLocaleTimeString()}.</h5>
+        </div>
+      );
+    }
+  }
+   
 ReactDOM.render(
     // sunset,
     <App />,
@@ -68,16 +139,17 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-
-
-
+ 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-/* Exemplo de uso de componentes 
+// Exemplo de uso de componentes 
+// como o codigo pode ser modularizado
+// e separado em pequenas partes
 
+/*
 function Avatar(props){
     return(
         <img className="Avatar"
